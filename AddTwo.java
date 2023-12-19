89b065dc325c057f865e3615ac018b5f3c1ef34c
ariel5rols <ariel.benavi@runi.ac.il>
2023-12-19 16:39:52 +0200

    Add awesome code for Homework 1

--- AddTwo.java
+++ AddTwo.java
@@ -2,7 +2,13 @@
  * Adds two given integers and prints the result in a fancy way.
  */
 public class AddTwo {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        int sum = num1 + num2;
+
+        // Print the result in a fancy way
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
 }
+
